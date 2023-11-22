0715e8ac730ee74fbe06dc699465f671c42edb70
IvanOrsh <ksironov@mail.ru>
2023-11-22 14:20:57 +0300

    feat: shared button

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,24 @@
+import { forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, disabled, type = "button", ...props }, ref) => (
+    <button
+      type={type}
+      ref={ref}
+      disabled={disabled}
+      className={twMerge(
+        "w-full rounded-full bg-zinc-600 border border-transparent px-3 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-bold hover:bg-zinc-700 transition duration-300",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+);
+Button.displayName = "Button";
+
+export default Button;
