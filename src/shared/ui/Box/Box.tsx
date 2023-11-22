b276f2590a5b6d79cddd467e7e64ecc752b12936
IvanOrsh <ksironov@mail.ru>
2023-11-22 12:19:26 +0300

    chore: box component

---
+++ src/shared/ui/Box/Box.tsx
@@ -0,0 +1,18 @@
+import { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
+
+type BoxProps = {
+  className?: string;
+};
+
+export default function Box({
+  children,
+  className,
+}: PropsWithChildren<BoxProps>) {
+  return (
+    <div className={twMerge("bg-zinc-900 rounded-lg h-fit w-full", className)}>
+      <h1>Box</h1>
+      {children}
+    </div>
+  );
+}
