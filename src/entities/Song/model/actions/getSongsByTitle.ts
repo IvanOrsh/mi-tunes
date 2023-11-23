611ad4b7eb507bef2886a872ad38bdff5a024ca1
IvanOrsh <ksironov@mail.ru>
2023-11-23 20:27:07 +0300

    feat: getSongsByTitle action

---
+++ src/entities/Song/model/actions/getSongsByTitle.ts
@@ -0,0 +1,28 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies, headers } from "next/headers";
+
+import type { Song } from "../types/song";
+import { getSongs } from "./getSongs";
+
+export const getSongsByTitle = async (title: string): Promise<Song[]> => {
+  const supabase = createServerComponentClient({
+    cookies,
+  });
+
+  if (!title) {
+    const allSongs = await getSongs();
+    return allSongs;
+  }
+
+  const { data, error } = await supabase
+    .from("songs")
+    .select("*")
+    .ilike("title", `%${title}%`)
+    .order("created_at", { ascending: false });
+
+  if (error) {
+    console.log(error);
+  }
+
+  return (data as any) || [];
+};
