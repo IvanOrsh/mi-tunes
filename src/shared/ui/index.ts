ec9133bcf28de18c5f5ae37db128de2e7c85b55d
IvanOrsh <ksironov@mail.ru>
2023-11-23 12:24:55 +0300

    feat: input component

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,3 +2,4 @@ export { default as Box } from "./Box/Box";
 export { default as Button } from "./Button/Button";
 export { default as ListItem } from "./ListItem/ListItem";
 export { default as Modal } from "./Modal/Modal";
+export { default as Input } from "./Input/Input";
