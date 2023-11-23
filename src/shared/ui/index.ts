62a2ebb8ba96ca8dc78eccd6c60c1d4f1874d8b4
IvanOrsh <ksironov@mail.ru>
2023-11-24 02:15:12 +0300

    feat: slider

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -4,3 +4,4 @@ export { default as ListItem } from "./ListItem/ListItem";
 export { default as Modal } from "./Modal/Modal";
 export { default as Input } from "./Input/Input";
 export { default as PlayButton } from "./PlayButton/PlayButton";
+export { default as Slider } from "./Slider/Slider";
