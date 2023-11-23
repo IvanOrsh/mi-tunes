c03656b0b39419d2c95e8477389029ffb6b58e44
IvanOrsh <ksironov@mail.ru>
2023-11-24 00:43:29 +0300

    fix: correct storage usage

--- src/features/playSong/model/hooks/useLoadSongUrl.ts
+++ src/features/playSong/model/hooks/useLoadSongUrl.ts
@@ -2,14 +2,14 @@ import { useSupabaseClient } from "@supabase/auth-helpers-react";
 
 import type { Song } from "@/entities/Song";
 
-export async function useLoadSongUrl(song: Song) {
+export function useLoadSongUrl(song: Song) {
   const supabaseClient = useSupabaseClient();
 
   if (!song) {
     return "";
   }
 
-  const { data: songData } = await supabaseClient.storage
+  const { data: songData } = supabaseClient.storage
     .from("songs")
     .getPublicUrl(song.song_path);
 
