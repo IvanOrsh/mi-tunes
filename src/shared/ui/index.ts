438e290a529c5cb865f66078dc2ec88bd996bb44
IvanOrsh <ksironov@mail.ru>
2023-11-23 20:12:42 +0300

    feat: play button

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -3,3 +3,4 @@ export { default as Button } from "./Button/Button";
 export { default as ListItem } from "./ListItem/ListItem";
 export { default as Modal } from "./Modal/Modal";
 export { default as Input } from "./Input/Input";
+export { default as PlayButton } from "./PlayButton/PlayButton";
